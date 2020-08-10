1abe79ca5430a3351b27ca4c0a090c5c277cc669
satyasaibhushan <satyasaibhushan@gmail.com>
2020-08-10 05:42:18 +0530

    Hashed password using bcrypt $ validated in login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../model/user");
-const { registrationValidation } = require("../validation");
+const { registrationValidation, loginValidation, isEmail } = require("../validation");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
   const { error } = registrationValidation(req.body);
@@ -13,16 +14,40 @@ router.post("/register", async (req, res) => {
   const doesUserexists = await User.findOne({ name: req.body.name });
   if (doesUserexists) return res.status(400).send("user name is taken.Please try a different one");
 
+  //Hashing of passwords
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
   let user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hash,
   });
   try {
-    const savedUser = await user.save();
-    res.send(savedUser);
+    await user.save();
+    res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //checking if given data is email/name
+  const { error: isEmailGivenError } = isEmail(req.body);
+  const user = !isEmailGivenError
+    ? await User.findOne({ email: req.body.loginID })
+    : await User.findOne({ name: req.body.loginID });
+
+  if (!user) return res.status(400).send("Email or password is invalid");
+  else {
+    //validating password
+    const validPassword = await bcrypt.compare(req.body.password,user.password)
+    if(!validPassword) return res.status(400).send("Email or password is invalid")
+    else res.send("logged in !!")
   }
 });
 
--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
 const joi = require("joi");
 
-const registrationValidation = (data) => {
+const registrationValidation = data => {
   const schema = joi.object({
     name: joi.string().min(6).required(),
     email: joi.string().min(6).required().email(),
@@ -10,16 +10,24 @@ const registrationValidation = (data) => {
   return schema.validate(data);
 };
 
-const loginValidation = (data) => {
-    const schema = joi.object({
-      name: joi.string().min(6).required(),
-      email: joi.string().min(6).required().email(),
-      password: joi.string().min(6).required(),
-    });
-  
-    return schema.validate(data);
-  };
+const loginValidation = data => {
+  const schema = joi.object({
+    loginID: joi.string().min(6).required(),
+    password: joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+const isEmail = data =>{
+  const schema = joi.object({
+    loginID: joi.string().min(6).required().email(),
+    password: joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+}
 
 module.exports.registrationValidation = registrationValidation;
 module.exports.loginValidation = loginValidation;
-
+module.exports.isEmail = isEmail;
