0f7bdd6a24204fca15f0663e1ba4b7f6b6fb8835
satyasaibhushan <satyasaibhushan@gmail.com>
2020-08-11 01:11:43 +0530

    Changed structure of the code to 2 folders

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require('http')
+const app = express();
+const server = http.createServer(app)
+const port = 3000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const socketio = require("socket.io");
+var io = socketio(server);
+
+dotenv.config();
+//DB Connection
+mongoose.connect(process.env.DB_LINK, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log("connected to DB");
+});
+//middleWares
+app.use(express.json());
+
+//import routes
+const authRoute = require("./routes/auth");
+
+//route middleWares
+app.use("/api/user", authRoute);
+
+io.on("connection",(socket)=>{console.log('new web socket connection')})
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
